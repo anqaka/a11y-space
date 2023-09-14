1c7f4ff534588a88df0d85b82c6fd45a58fa1e86
Ania <anna.karon@snow.dog>
2023-09-15 01:54:55 +0200

    more tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -12,6 +12,17 @@ describe('home page', () => {
     cy.getByData('header-navigation').get('ul > li').eq(0).contains('Home')
   })
 
+  it('Dialog opens with trigger and move focus to content and move back after closing', () => {
+    cy.getByData('dialog-trigger').click()
+    cy.getByData('dialog-content').then(($el) => {
+      Cypress.dom.isFocused($el)
+    }).get('button[aria-label="close modal"]').click()
+    cy.getByData('dialog-trigger').then(($el) => {
+      Cypress.dom.isFocused($el)
+    })
+
+  })
+
   context('Live region page', () => {
     it('Live region page is accessible', () => {
       cy.getByData('header-navigation').get('ul > li').find('a').contains('Live region').click()
