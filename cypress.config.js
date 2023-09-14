302cd8fca33dd0100d7cb275b37a554f6bf194cd
Ania <anna.karon@snow.dog>
2023-09-15 00:52:02 +0200

    add cypress with gh actions and axe watcher

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "cypress";
+import { cypressConfig } from '@axe-core/watcher';
+import dotenv from 'dotenv';
+dotenv.config()
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+const { AXE_API_KEY } = process.env
+
+export default defineConfig(
+  cypressConfig({
+    axe: {
+      apiKey: AXE_API_KEY,
     },
-  },
-});
+    defaultCommandTimeout: 10000,
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
+    },
+  })
+);
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,11 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@axe-core/watcher/dist/cypressCommands';
+
+afterEach(() => {
+  cy.axeFlush();
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
